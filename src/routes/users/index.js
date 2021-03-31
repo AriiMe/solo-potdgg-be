861a468fc856476eb6785621db340a6ee91f2950
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-03-31 13:24:57 +0200

    up

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -11,10 +11,10 @@ const cloudinary = require("../../cloudinary");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const sgMail = require("@sendgrid/mail");
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "potd-profilepics",
-  },
+    cloudinary: cloudinary,
+    params: {
+        folder: "potd-profilepics",
+    },
 });
 const cloudinaryMulter = multer({ storage: storage });
 const jwt = require("jsonwebtoken");
@@ -22,249 +22,249 @@ const { authenticate, refreshToken } = require("../../auth");
 const router = require("express").Router();
 
 router.route("/register").post(async (req, res, next) => {
-  try {
-    const newUser = await User.create({
-      ...req.body,
-      imgurl:
-        "https://res.cloudinary.com/dhmw620tl/image/upload/v1611844643/benchmark3/i91vqe984yfdir5xp8xh.png",
-    });
-    res.send(newUser);
-    if (newUser) {
-      sgMail.setApiKey(process.env.SEND_GRID_KEY);
-      const msg = {
-        to: req.body.email, // Change to your recipient
-        from: "support@potd.lol", // Change to your verified sender
-        subject: `Welcome aboard ${req.body.username}! `,
-        text: "Thank you for joining potd.lol have a great time here",
-        html:
-          "<strong>Thank you for joining potd.lol have a great time here</strong>",
-        templateId: "d-f1409979d7824aa19e1ca007f51eebff",
-      };
-      sgMail
-        .send(msg)
-        .then(() => {
-          console.log("Email sent");
-          res.send("request finished");
-        })
-        .catch((error) => {
-          console.error(error);
+    try {
+        const newUser = await User.create({
+            ...req.body,
+            imgurl:
+                "https://res.cloudinary.com/dhmw620tl/image/upload/v1611844643/benchmark3/i91vqe984yfdir5xp8xh.png",
         });
+        res.send(newUser);
+        if (newUser) {
+            sgMail.setApiKey(process.env.SEND_GRID_KEY);
+            const msg = {
+                to: req.body.email, // Change to your recipient
+                from: "support@potd.lol", // Change to your verified sender
+                subject: `Welcome aboard ${req.body.username}! `,
+                text: "Thank you for joining potd.lol have a great time here",
+                html:
+                    "<strong>Thank you for joining potd.lol have a great time here</strong>",
+                templateId: "d-f1409979d7824aa19e1ca007f51eebff",
+            };
+            sgMail
+                .send(msg)
+                .then(() => {
+                    console.log("Email sent");
+                    res.send("request finished");
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    } catch (error) {
+        console.log(error);
+        next(error);
     }
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 });
 
 router.route("/login").post(async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ where: { username } });
-    if (user) {
-      const isMatch = user.validPassword(password);
-      if (isMatch) {
-        const accessToken = await jwt.sign(
-          { id: user.id },
-          process.env.JWT_KEY,
-          { expiresIn: "30m" }
-        );
-        const refreshToken = await jwt.sign(
-          { id: user.id },
-          process.env.JWT_REFRESH_KEY,
-          { expiresIn: "1w" }
-        );
-        res.cookie("accessToken", accessToken, {
-          httpOnly: true,
-          secure: true, //set to true when deploy
-          sameSite: "none", //set to none when deploy
-        });
-        res.cookie("refreshToken", refreshToken, {
-          httpOnly: true,
-          secure: true, //set to true when deploy
-          sameSite: "none", //set to none when deploy
-        });
-        res.send(user);
-      } else {
-        res.status(401).send("Incorret Username or Password");
-      }
-    } else {
-      res.status(401).send("Incorret Username or Password");
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ where: { username } });
+        if (user) {
+            const isMatch = user.validPassword(password);
+            if (isMatch) {
+                const accessToken = await jwt.sign(
+                    { id: user.id },
+                    process.env.JWT_KEY,
+                    { expiresIn: "30m" }
+                );
+                const refreshToken = await jwt.sign(
+                    { id: user.id },
+                    process.env.JWT_REFRESH_KEY,
+                    { expiresIn: "1w" }
+                );
+                res.cookie("accessToken", accessToken, {
+                    httpOnly: true,
+                    secure: true, //set to true when deploy
+                    sameSite: "none", //set to none when deploy
+                });
+                res.cookie("refreshToken", refreshToken, {
+                    httpOnly: true,
+                    secure: true, //set to true when deploy
+                    sameSite: "none", //set to none when deploy
+                });
+                res.send(user);
+            } else {
+                res.status(401).send("Incorret Username or Password");
+            }
+        } else {
+            res.status(401).send("Incorret Username or Password");
+        }
+    } catch (error) {
+        console.log(error);
+        next(error);
     }
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 });
 router.post("/logout", authenticate, async (req, res, next) => {
-  try {
-    req.user.refresh_tokens = req.user.refresh_tokens.filter(
-      (t) => t !== req.cookies.refreshToken
-    );
-    await User.update(
-      { refresh_tokens: req.user.refresh_tokens },
-      { where: { id: req.user.id } }
-    );
+    try {
+        req.user.JWT_REFRESH_KEY = req.user.JWT_REFRESH_KEY.filter(
+            (t) => t !== req.cookies.JWT_REFRESH_KEY
+        );
+        await User.update(
+            { JWT_REFRESH_KEY: req.user.JWT_REFRESH_KEY },
+            { where: { id: req.user.id } }
+        );
 
-    res.clearCookie("accessToken");
-    res.clearCookie("refreshToken");
-    res.status(200).send();
-  } catch (e) {
-    console.log(e);
-    next(e);
-  }
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken");
+        res.status(200).send();
+    } catch (e) {
+        console.log(e);
+        next(e);
+    }
 });
 
 router.get("/me", authenticate, async (req, res, next) => {
-  try {
-    const singleUser = await User.findByPk(req.user.dataValues.id, {
-      include: [
-        Post,
-        { model: Stalk, include: [{ model: User, as: "stalking" }] },
-        { model: Stalker, include: [{ model: User, as: "stalker" }] },
-        Tagged,
-        SavedPost,
-      ],
-    });
-    res.send(singleUser);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Something went wrong!");
-  }
+    try {
+        const singleUser = await User.findByPk(req.user.dataValues.id, {
+            include: [
+                Post,
+                { model: Stalk, include: [{ model: User, as: "stalking" }] },
+                { model: Stalker, include: [{ model: User, as: "stalker" }] },
+                Tagged,
+                SavedPost,
+            ],
+        });
+        res.send(singleUser);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Something went wrong!");
+    }
 });
 
 router.get("/", authenticate, async (req, res) => {
-  try {
-    const allUser = await User.findAll({
-      include: [
-        Post,
-        { model: Stalk, include: [{ model: User, as: "stalking" }] },
-        { model: Stalker, include: [{ model: User, as: "stalker" }] },
-        Tagged,
-      ],
-    });
-    res.send(allUser);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Something went wrong!");
-  }
+    try {
+        const allUser = await User.findAll({
+            include: [
+                Post,
+                { model: Stalk, include: [{ model: User, as: "stalking" }] },
+                { model: Stalker, include: [{ model: User, as: "stalker" }] },
+                Tagged,
+            ],
+        });
+        res.send(allUser);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Something went wrong!");
+    }
 });
 
 router.get("/:id", authenticate, async (req, res) => {
-  try {
-    if (req.user.dataValues.id.toString() === req.params.id) {
-      const singleUser = await User.findByPk(req.params.id, {
-        include: [
-          Post,
-          { model: Stalk, include: [{ model: User, as: "stalking" }] },
-          { model: Stalker, include: [{ model: User, as: "stalker" }] },
-          Tagged,
-          SavedPost,
-        ],
-      });
-      res.send(singleUser);
-    } else {
-      const singleUser = await User.findByPk(req.params.id, {
-        include: [Post, Stalk, Stalker, Tagged],
-      });
-      if (singleUser) {
-        res.send(singleUser);
-      } else {
-        res.status(404).send("User not found within database");
-      }
+    try {
+        if (req.user.dataValues.id.toString() === req.params.id) {
+            const singleUser = await User.findByPk(req.params.id, {
+                include: [
+                    Post,
+                    { model: Stalk, include: [{ model: User, as: "stalking" }] },
+                    { model: Stalker, include: [{ model: User, as: "stalker" }] },
+                    Tagged,
+                    SavedPost,
+                ],
+            });
+            res.send(singleUser);
+        } else {
+            const singleUser = await User.findByPk(req.params.id, {
+                include: [Post, Stalk, Stalker, Tagged],
+            });
+            if (singleUser) {
+                res.send(singleUser);
+            } else {
+                res.status(404).send("User not found within database");
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Something went wrong!");
     }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Something went wrong!");
-  }
 });
 
 router.put("/:id", authenticate, async (req, res) => {
-  try {
-    if (req.user.dataValues.id.toString() === req.params.id) {
-      const alteredUser = await User.update(req.body, {
-        where: { id: req.params.id },
-        include: [Post, Stalk, Stalker, Tagged],
-        returning: true,
-      });
-      res.send(alteredUser);
-    } else {
-      res.status(401).send("Unauthorized: This is not your account!");
+    try {
+        if (req.user.dataValues.id.toString() === req.params.id) {
+            const alteredUser = await User.update(req.body, {
+                where: { id: req.params.id },
+                include: [Post, Stalk, Stalker, Tagged],
+                returning: true,
+            });
+            res.send(alteredUser);
+        } else {
+            res.status(401).send("Unauthorized: This is not your account!");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Something went wrong!");
     }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Something went wrong!");
-  }
 });
 
 router.put(
-  "/:id/upload",
-  authenticate,
-  cloudinaryMulter.single("ProfilePic"),
-  async (req, res) => {
-    try {
-      if (req.user.dataValues.id.toString() === req.params.id) {
-        const alteredIMG = await User.update(
-          { ...req.body, imgurl: req.file.path },
-          {
-            where: { id: req.params.id },
-            returning: true,
-          }
-        );
-        res.send(alteredIMG);
-      } else {
-        res.status(401).send("Unauthorized: This is not your account!");
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).send("Something went bad!");
+    "/:id/upload",
+    authenticate,
+    cloudinaryMulter.single("ProfilePic"),
+    async (req, res) => {
+        try {
+            if (req.user.dataValues.id.toString() === req.params.id) {
+                const alteredIMG = await User.update(
+                    { ...req.body, imgurl: req.file.path },
+                    {
+                        where: { id: req.params.id },
+                        returning: true,
+                    }
+                );
+                res.send(alteredIMG);
+            } else {
+                res.status(401).send("Unauthorized: This is not your account!");
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).send("Something went bad!");
+        }
     }
-  }
 );
 router.put(
-  "/:id/upload/register",
-  cloudinaryMulter.single("ProfilePic"),
-  async (req, res) => {
-    try {
-      const user = await User.findByPk(req.params.id);
-      if (
-        user.dataValues.imgurl ===
-        "https://res.cloudinary.com/dhmw620tl/image/upload/v1611844643/benchmark3/i91vqe984yfdir5xp8xh.png"
-      ) {
-        const alteredIMG = await User.update(
-          { ...req.body, imgurl: req.file.path },
-          {
-            where: { id: req.params.id },
-            returning: true,
-          }
-        );
-        res.send(alteredIMG);
-      } else {
-        res.status(401).send("Unauthorized: This is not your account!");
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).send("Something went bad!");
+    "/:id/upload/register",
+    cloudinaryMulter.single("ProfilePic"),
+    async (req, res) => {
+        try {
+            const user = await User.findByPk(req.params.id);
+            if (
+                user.dataValues.imgurl ===
+                "https://res.cloudinary.com/dhmw620tl/image/upload/v1611844643/benchmark3/i91vqe984yfdir5xp8xh.png"
+            ) {
+                const alteredIMG = await User.update(
+                    { ...req.body, imgurl: req.file.path },
+                    {
+                        where: { id: req.params.id },
+                        returning: true,
+                    }
+                );
+                res.send(alteredIMG);
+            } else {
+                res.status(401).send("Unauthorized: This is not your account!");
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).send("Something went bad!");
+        }
     }
-  }
 );
 
 router.route("/refresh/token").post(async (req, res, next) => {
-  try {
-    const refreshToken = req.cookies.refreshToken;
-    const newTokens = await refreshToken(refreshToken);
-    console.log(newTokens);
-    res.cookie("accessToken", newTokens.accessToken, {
-      httpOnly: true,
-    });
-    res.cookie("refreshToken", newTokens.refreshToken, {
-      httpOnly: true,
-      path: "/insta/users/refresh/token",
-    });
-    res.send("Tokens Regenrated!");
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        const newTokens = await refreshToken(refreshToken);
+        console.log(newTokens);
+        res.cookie("accessToken", newTokens.accessToken, {
+            httpOnly: true,
+        });
+        res.cookie("refreshToken", newTokens.refreshToken, {
+            httpOnly: true,
+            path: "/insta/users/refresh/token",
+        });
+        res.send("Tokens Regenrated!");
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
 });
 
 module.exports = router;
